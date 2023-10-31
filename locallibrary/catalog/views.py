b9e27b4686be417f54f9c048b6549e69493a5ce7
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2023-10-31 10:53:15 +0700

    borrowed books page change

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 
 
@@ -50,7 +51,6 @@ class AuthorDetailView(generic.DetailView):
 
 
 
-
 class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     """
     Generic class-based view listing books on loan to current user.
@@ -62,3 +62,12 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
+
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')

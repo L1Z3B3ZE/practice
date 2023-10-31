1980d7767606388c9fe4b4373aaa27e56bfd537d
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2023-10-31 21:00:37 +0700

    get number of genres and the number of books that contain any word in their titles

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -13,6 +13,11 @@ def index(request):
     # Available copies of books
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
+    num_genres = Genre.objects.count()
+
+    search_word = request.GET.get('search_word', '')
+
+    num_books_with_word = Book.objects.filter(title__icontains=search_word).count()
 
     # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 1)
@@ -22,9 +27,15 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
-                 'num_visits': num_visits},
+        context={'num_books': num_books,
+                 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors,
+                 'num_genres': num_genres,
+                 'num_books_with_word': num_books_with_word,
+                 'search_word': search_word,
+                 'num_visits': num_visits,
+                 }
     )
 
 

bed8a941dece4b75bc37d541c933f954c3c987f0
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2023-10-31 11:16:32 +0700

    count genres realisation

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,6 +12,7 @@ def index(request):
     """
     # Генерация "количеств" некоторых главных объектов
     num_books=Book.objects.all().count()
+    num_genres = Genre.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
@@ -28,7 +29,9 @@ def index(request):
                  'num_instances':num_instances,
                  'num_instances_available':num_instances_available,
                  'num_authors':num_authors,
-                 'num_visits':num_visits},
+                 'num_visits':num_visits,
+                 'num_genres': num_genres,
+                 },
     )
 
 
